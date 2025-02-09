4e76134dd5b7c2a3acf109f335d7dc97c196ad48
1mono2 <taka19980618@gmail.com>
2025-02-10 03:51:28 +0900

    fix: ddd document

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 
 const connectionString = process.env.POSTGRES_URL;
-const db = drizzle(connectionString);
+export const db = drizzle(connectionString);
 
 export default db;
